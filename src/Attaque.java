ba101a66371a78789356b4e65cc8111e0082de56
Loïc Chibrac <loic.chibrac@etu.u-bordeaux.fr>
2015-06-26 18:19:03 +0200

    preparation de l'ajout des attaques en dur

--- src/Attaque.java
+++ src/Attaque.java
@@ -1,16 +1,18 @@
 public class Attaque {
 	
-	public Attaque(String nom, Type t, int precision,int priorite){
+	public Attaque(String nom, Type t, int precision, int pp, int priorite){
 		_nom = new String(nom);
 		_t = t.clone();
 		_precision = precision;
+		_pp = pp;
 		_priorite = priorite;
 	}
 	
-	public Attaque(String nom, Type t, int precision){
+	public Attaque(String nom, Type t, int precision, int pp){
 		_nom = new String(nom);
 		_t = t.clone();
 		_precision = precision;
+		_pp = pp;
 		_priorite = 1;
 	}
 	
@@ -19,12 +21,10 @@ public class Attaque {
 	public PokemonEnCombat _lanceur;
 	public PokemonEnCombat _cible;
 	public int _precision;
+	public int _pp;
 	public int _priorite;
 	
-	public static final Attaque toxic = new Attaque("Toxic", Type.poison, 90);
-	public static final Attaque repos = new Attaque("Repos", Type.psy, 100);
-	
-	
+
 	
 	
 	public void effet(PokemonEnCombat lanceur, PokemonEnCombat cible){
@@ -47,15 +47,11 @@ public class Attaque {
 		}
 	}
 	
-	public static Attaque berceuse = new Attaque("Berceuse", Type.normal , 50);
+	
 	
 	public static void main(String[] argv){
 		
-		
-		
-		//PokemonEnCombat grodoudou = new PokemonEnCombat(Pokemon1G.grodoudou, new Attaque[]{berceuse,null,null,null},0, new int[]{484,158,126,206,218,126},100);
-		//PokemonEnCombat prismillon = new PokemonEnCombat(Pokemon6G.prismillon,new Attaque[]{null,null,null,null},0,new int[]{301,126,136,306,136,277}, 100);
-		
+	
 	}
 	
 	
--- src/AttaqueDegat.java
+++ src/AttaqueDegat.java
@@ -1,15 +1,15 @@
 
 public class AttaqueDegat extends Attaque{
 	
-	public AttaqueDegat(String nom, Type t, int precision, int puissance, int effet,int categorie){
-		super(nom,t,precision);
+	public AttaqueDegat(String nom, Type t, int categorie, int puissance, int precision, int pp, int effet){
+		super(nom,t,precision,pp);
 		_puissance = puissance;
 		_effet = effet;
 		_categorie = categorie;
 	}
 	
-	public AttaqueDegat(String nom, Type t, int precision, int priorite, int puissance, int effet,int categorie){
-		super(nom,t,precision,priorite);
+	public AttaqueDegat(String nom, Type t, int categorie, int puissance, int precision, int pp, int effet, int priorite){
+		super(nom,t,precision,pp,priorite);
 		_puissance = puissance;
 		_effet = effet;
 		_categorie = categorie;
@@ -31,15 +31,7 @@ public class AttaqueDegat extends Attaque{
 			return new String("Special");
 		}
 	}
-	
-	
-	public static final AttaqueDegat laserglace = new AttaqueDegat("Laser Glace", Type.glace, 100, 90, 10, 1);
-	public static final AttaqueDegat lanceflamme = new AttaqueDegat("Lance-Flamme", Type.feu, 100, 90, 10, 1);
-	public static final AttaqueDegat bombbeurk =  new AttaqueDegat("Bomb-Beurk", Type.poison, 100, 90, 30, 1);
-	public static final AttaqueDegat harcelement = new AttaqueDegat("Harcelement", Type.insecte, 100, 20, 100, 1);
-	public static final AttaqueDegat surf = new AttaqueDegat("Surf", Type.eau, 100, 95, 0, 1);
 
-	
 	public void effet(PokemonEnCombat lanceur, PokemonEnCombat cible){
 		String s = this._nom;
 		int x = Probabilite.random(0, 100);
@@ -70,14 +62,5 @@ public class AttaqueDegat extends Attaque{
 	
 	public static void main (String[] argv){
 		
-		PokemonEnCombat smogogo = new PokemonEnCombat(Pokemon1G.smogogo, new Attaque[]{null, null, null,null},0,new int[]{334,194,339,226,176,156},100);
-		PokemonEnCombat noacier = new PokemonEnCombat(Pokemon5G.noacier, new Attaque[]{null, null, null, null},0,new int[]{352,246,298,129,331,76}, 100);
-		
-		AttaqueDegat lanceflamme = new AttaqueDegat("Lance-Flamme", Type.feu, 90, 100, 10, 1);
-		
-		for (int i = 0 ; i < 100 ; ++i){
-			System.out.println(Probabilite.degats(lanceflamme, smogogo, noacier));
-
-		}
 	}
 }
